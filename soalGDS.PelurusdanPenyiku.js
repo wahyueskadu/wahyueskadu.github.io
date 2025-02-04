f1a8dc48f071632f015ff870a7ac58f7a29d4a87
wahyueskadu <septya.wahyu70@guru.smp.belajar.id>
2025-02-04 14:07:59 +0700

    Update soalGDS.PelurusdanPenyiku.js

--- soalGDS.PelurusdanPenyiku.js
+++ soalGDS.PelurusdanPenyiku.js
@@ -14,21 +14,27 @@ function getSoal(x){
     case 1:
         let pelurus = getIntegerNZ(10,170);
         ans = 180 - pelurus;
-        makeBubbleBot(`Pelurus dari ${pelurus} adalah .... `);
+        list = ['pelurus','suplemen'];
+        displayType = list[getInteger(0,1)];
+        makeBubbleBot(`${displayType} dari ${pelurus} adalah .... `);
         break
     case 2:
         let penyiku = getIntegerNZ(10,80);
         ans = 90 - penyiku;
-        makeBubbleBot(`Penyiku dari ${penyiku} adalah ....`);
+        list = ['penyiku','komplemen'];
+        displayType = list[getInteger(0,1)];
+        makeBubbleBot(`${displayType} dari ${penyiku} adalah ....`);
         break
     case 3:
         let bertoklakbelakang = getIntegerNZ(10,170);
         makeBubbleBot(`Sudut bertolak belakang dari ${bertoklakbelakang} adalah ....`);
         ans = bertoklakbelakang
+        displayType = 'bertolak belakang'
         break
     } 
     qArray.displayAnswer = ans;
     qArray.tipesoal = ans;
+    qArray.displayType = displayType;
 }
 
 
@@ -41,10 +47,10 @@ function getHelp(){
     } else {
         switch (tipesoal){
             case 1:
-              resp = `Ingat, sudut penyiku jika dijumlahkan hasilnya 90`;
+              resp = `Ingat, sudut ${qArray.displayType} jika dijumlahkan hasilnya 180`;
               break
             case 2:
-              resp = `Ingat, sudut pelurus jika dijumlahkan hasilnya 180`;
+              resp = `Ingat, sudut ${qArray.displayType} jika dijumlahkan hasilnya 90`;
               break
             case 3:
               resp = `Ingat, sudut bertolak belakang besarnya sama`;
